dffa42281ce7074dd9513662a418132d69e9bd48
Thanh Vu <csnsjh@gmail.com>
2021-09-10 01:39:40 +0700

    update

---
+++ features/lobby/components/JoinForm.tsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Button,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography
+} from '@material-ui/core';
+import { useRouter } from 'next/router';
+import React, { FC } from 'react';
+import en from 'translations/en/lobby';
+import vi from 'translations/vi/lobby';
+
+const useStyles = makeStyles((theme) => ({
+  formRoot: {
+    width: '350px',
+    backgroundColor: theme.palette.background.paper,
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  formContainer: {
+    width: '100%',
+  },
+  formTitle: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  link: {
+    textDecoration: 'underline',
+  },
+}));
+
+export const JoinForm: FC = () => {
+  const { locale } = useRouter();
+  const t = locale === 'vi' ? vi : en;
+  const classes = useStyles();
+
+  return (
+    <Paper elevation={0} className={classes.formRoot}>
+      <form className={classes.formContainer}>
+        <Box mb={1.5}>
+          <Typography variant="h5" className={classes.formTitle}>
+            {t.join}
+          </Typography>
+        </Box>
+        <Box>
+          <TextField
+            label={t.code}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+          />
+        </Box>
+
+        <Box mt={1.5}>
+          <Button variant="contained" color="primary" size="large" fullWidth>
+            {t.confirm}
+          </Button>
+        </Box>
+      </form>
+    </Paper>
+  );
+};
