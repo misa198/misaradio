ace069033de923fa1310c94a29ad792ae8753dec
Thanh Vu <csnsjh@gmail.com>
2021-09-09 21:57:20 +0700

    banner

--- translations/en/banner.ts
+++ translations/en/banner.ts
@@ -1,4 +1,4 @@
 export default {
-  bannerTitle: 'Unlimited music, Youtube, SoundCloud',
+  bannerTitle: 'Unlimited music with Youtube and SoundCloud',
   bannerSubtitle: 'Login to join with your friends',
 };
