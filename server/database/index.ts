8e9341ea908c346e6df9be82d95f7a6af17e5b16
Thanh Vu <csnsjh@gmail.com>
2021-09-10 08:12:27 +0700

    update

--- server/database/index.ts
+++ server/database/index.ts
@@ -3,8 +3,15 @@ import mongoose from 'mongoose';
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017';
 
 export default function connectDb() {
-  mongoose.connect(dbUrl, {}, (err) => {
-    if (err) console.log(err);
-    else console.log('> Connected to database');
-  });
+  mongoose.connect(
+    dbUrl,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => {
+      if (err) console.log(err);
+      else console.log('> Connected to database');
+    },
+  );
 }
