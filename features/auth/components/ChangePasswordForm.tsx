8ff48aecb88ea68c49e029b12783d4ebd18edcc0
Thanh Vu <csnsjh@gmail.com>
2021-09-11 05:33:37 +0700

    change password

--- features/auth/components/ChangePasswordForm.tsx
+++ features/auth/components/ChangePasswordForm.tsx
@@ -1,16 +1,21 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import {
   Box,
   Button,
+  LinearProgress,
   makeStyles,
   Paper,
   TextField,
   Typography,
-  LinearProgress,
 } from '@material-ui/core';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 import { useRouter } from 'next/router';
 import React, { FC } from 'react';
+import { useForm } from 'react-hook-form';
 import en from 'translations/en/auth';
 import vi from 'translations/vi/auth';
+import * as yup from 'yup';
+import { changePassword } from '../authThunk';
 
 const useStyles = makeStyles((theme) => ({
   formRoot: {
@@ -37,17 +42,58 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+interface ChangePasswordFormFields {
+  newPassword: string;
+  password: string;
+  email: string;
+}
+
 export const ChangePasswordForm: FC = () => {
   const { locale } = useRouter();
   const t = locale === 'vi' ? vi : en;
   const classes = useStyles();
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector((state) => state.auth.changePassword.loading);
+
+  const schema = yup.object().shape({
+    email: yup.string().email(t.emailValidation).required(t.requiredValidation),
+    password: yup
+      .string()
+      .min(6, t.passwordValidation)
+      .required(t.requiredValidation),
+    newPassword: yup
+      .string()
+      .min(6, t.passwordValidation)
+      .required(t.requiredValidation),
+    confirmNewPassword: yup
+      .string()
+      .oneOf([yup.ref('newPassword'), null], t.confirmPasswordValidation),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit(data: ChangePasswordFormFields) {
+    dispatch(
+      changePassword({
+        email: data.email,
+        password: data.password,
+        newPassword: data.newPassword,
+      }),
+    );
+  }
 
   return (
     <Paper elevation={0} className={classes.formRoot}>
       <Box width="100%" className={classes.loading}>
-        <LinearProgress color="primary" />
+        {loading && <LinearProgress color="primary" />}
       </Box>
-      <form className={classes.formContainer}>
+      <form onSubmit={handleSubmit(onSubmit)} className={classes.formContainer}>
         <Box mb={1.5}>
           <Typography variant="h5" className={classes.formTitle}>
             {t.changePassword}
@@ -59,6 +105,8 @@ export const ChangePasswordForm: FC = () => {
             margin="normal"
             variant="outlined"
             fullWidth
+            {...register('email')}
+            helperText={errors.email?.message}
           />
         </Box>
         <Box>
@@ -66,8 +114,10 @@ export const ChangePasswordForm: FC = () => {
             label={t.oldPassword}
             margin="normal"
             variant="outlined"
-            type="password"
             fullWidth
+            type="password"
+            {...register('password')}
+            helperText={errors.password?.message}
           />
         </Box>
         <Box>
@@ -77,6 +127,8 @@ export const ChangePasswordForm: FC = () => {
             variant="outlined"
             type="password"
             fullWidth
+            {...register('newPassword')}
+            helperText={errors.newPassword?.message}
           />
         </Box>
         <Box>
@@ -86,11 +138,19 @@ export const ChangePasswordForm: FC = () => {
             variant="outlined"
             type="password"
             fullWidth
+            {...register('confirmNewPassword')}
+            helperText={errors.confirmNewPassword?.message}
           />
         </Box>
 
         <Box mt={1.5}>
-          <Button variant="contained" color="primary" size="large" fullWidth>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            fullWidth
+            type="submit"
+          >
             {t.confirm}
           </Button>
         </Box>
