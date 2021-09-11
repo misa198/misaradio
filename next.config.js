773b53cb45963078d718e0b94e88c327ebf89f05
Thanh Vu <csnsjh@gmail.com>
2021-09-12 06:42:18 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'vi'],
-    defaultLocale: 'vi',
+    defaultLocale: 'en',
     localeDetection: true,
-  },
-  images: {
-    domains: ['i.ytimg.com'],
   },
 };
