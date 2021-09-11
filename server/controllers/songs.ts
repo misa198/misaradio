00ba5e8c1452359ac80b4080f5a216a090d7d40e
Thanh Vu <csnsjh@gmail.com>
2021-09-12 06:32:36 +0700

    update

--- server/controllers/songs.ts
+++ server/controllers/songs.ts
@@ -5,18 +5,22 @@ import { Song } from '../types/Song';
 import { UserRequest } from '../types/UserRequest';
 
 export const searchSongs = async (req: Request, res: Response) => {
-  const user = (req as UserRequest).user;
-  const { type, query } = req.query;
-  let result: Song[] = [];
-  switch (type) {
-    case 'youtube':
-      result = await youtubeService.search(query as string, user.name);
-      break;
-    case 'soundcloud':
-      result = await soundcloudService.search(query as string, user.name);
-      break;
-    default:
-      break;
+  try {
+    const user = (req as UserRequest).user;
+    const { type, query } = req.query;
+    let result: Song[] = [];
+    switch (type) {
+      case 'youtube':
+        result = await youtubeService.search(query as string, user.name);
+        break;
+      case 'soundcloud':
+        result = await soundcloudService.search(query as string, user.name);
+        break;
+      default:
+        break;
+    }
+    return res.send({ data: result });
+  } catch (e) {
+    return res.status(500).send({ error: 'Error' });
   }
-  return res.send({ data: result });
 };
