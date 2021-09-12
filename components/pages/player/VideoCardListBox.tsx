87bca365fbdf8e71989779a9611cc473b86985a1
Thanh Vu <csnsjh@gmail.com>
2021-09-12 23:43:33 +0700

    order list

--- components/pages/player/VideoCardListBox.tsx
+++ components/pages/player/VideoCardListBox.tsx
@@ -4,11 +4,30 @@ import { useRouter } from 'next/router';
 import React, { FC } from 'react';
 import en from 'translations/en/player';
 import vi from 'translations/vi/player';
+import { useAppSelector } from 'app/hooks';
+import { SongCard } from '.';
 
 const useStyles = makeStyles(() => ({
   videoListBox: {
     padding: '0 !important',
     maxHeight: '65vh',
+  },
+  itemsWrapper: {
+    overflowX: 'hidden',
+    overflowY: 'auto',
+
+    '&::-webkit-scrollbar': {
+      width: '3px',
+    },
+    '&::-webkit-scrollbar-track': {
+      boxShadow: 'inset 0 0 6px #202020',
+      webkitBoxShadow: 'inset 0 0 6px #202020',
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: '#555',
+      outline: 'none',
+      borderRadius: '10px',
+    },
   },
 }));
 
@@ -20,19 +39,29 @@ export const VideoCardListBox: FC<PropTypes> = ({ handleOpen }) => {
   const { locale } = useRouter();
   const t = locale === 'vi' ? vi : en;
   const classes = useStyles();
+  const queue = useAppSelector((state) => state.player.room?.queue);
 
   return (
     <Box display="flex" flexDirection="column" className={classes.videoListBox}>
-      <Box flexGrow={1} width="100%">
-        {/* <SongCard />
-        <SongCard />
-        <SongCard />
-        <SongCard />
-        <SongCard /> */}
-        <Box width="100%" pt={2} pb={2}>
-          <MusicNote />
-          <Typography variant="body1">{t.addMessage}</Typography>
-        </Box>
+      <Box flexGrow={1} width="100%" className={classes.itemsWrapper}>
+        {!queue || queue?.length === 0 ? (
+          <Box width="100%" pt={2} pb={2}>
+            <MusicNote />
+            <Typography variant="body1">{t.addMessage}</Typography>
+          </Box>
+        ) : (
+          <>
+            {queue.map((song) => (
+              <>
+                <SongCard song={song} key={song.uniqueId} />
+                <SongCard song={song} key={song.uniqueId} />
+                <SongCard song={song} key={song.uniqueId} />
+                <SongCard song={song} key={song.uniqueId} />
+                <SongCard song={song} key={song.uniqueId} />
+              </>
+            ))}
+          </>
+        )}
       </Box>
       <Box pt={1}>
         <Button
